ae55013967213da0f0a600f4459ba203dd02d284
njayman <najishm828282@gmail.com>
2021-09-03 12:53:28 +0600

    project done

--- pages/admin/products/index.jsx
+++ pages/admin/products/index.jsx
@@ -5,3 +5,17 @@ const ProductsAdmin = () => {
 };
 
 export default ProductsAdmin;
+// export async function getStaticPaths({ req }) {
+//   // Fetch data from external API
+//   const baseUrl = req ? `${req.protocol}://${req.get("Host")}` : "";
+//   console.log(baseUrl);
+//   const res = await fetch(`http://localhost:3000/api/admin/products`);
+//   const data = await res.json();
+//   const paths = data.products.map((product) => ({
+//     params: { id: product._id },
+//   }));
+
+//   // We'll pre-render only these paths at build time.
+//   // { fallback: false } means other routes should 404.
+//   return { paths, fallback: false };
+// }
